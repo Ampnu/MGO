1e06b62cac616864753f15b1f52cfa349e061527
DESKTOP-V0URUNG\Tony <tonynewsom76@gmail.com>
2018-02-03 19:13:03 -0500

    Milestone Commit: First Level Staging, Character Controls and Orb Collection

---
+++ Momo Alpha v1/Assets/Script/JumpBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBox : MonoBehaviour {
+
+    public bool jump = false;
+    public Transform player;
+    public Animator anim;
+    public float jumpSpeed = 100f;
+    //public float gravity = 9.81F;
+
+    //private Vector3 jumpDirection;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            print("JUMP");
+            player.transform.TransformDirection(0,jumpSpeed,0);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("LAND");
+        }
+    }
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Momo Alpha v1/Assets/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float turnSpeed = 100;
+    //public float jumpSpeed = 8.0F;
+    public float gravity = 9.81F;
+    
+
+    private Vector3 moveDirection = Vector3.zero;    
+    private CharacterController controller;
+    private float horz, vert;
+  
+
+    void Start()
+    {    
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        MovePlayer(horz, vert);
+
+    }
+
+    void MovePlayer(float h, float v)
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(h, 0, v);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            //if (isJump)
+            //{
+            //    moveDirection.y = jumpSpeed;
+            //}
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+        
+    }
+
+}
