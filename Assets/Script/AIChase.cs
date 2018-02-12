7d3ff8d6a6fbc95ab9dc6eefa3938788ddae37f2
TONY-LAPTOP <tonynewsom76@gmail.com>
2018-02-12 07:16:17 -0500

    Added Spawner

--- Assets/Script/AIChase.cs
+++ Assets/Script/AIChase.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIController : MonoBehaviour
+public class AIChase : MonoBehaviour
 {
     private Transform player;               // Reference to the player's position.
-    private NavMeshAgent enemy;               // Reference to the nav mesh agent.
-
+    private NavMeshAgent enemy;             // Reference to the nav mesh agent.
 
     // Use this for initialization
-    void Start()
+    void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
-
-    // Update is called once per frame
-    void Update()
+	
+	// Update is called once per frame
+	void Update ()
     {
         enemy.SetDestination(player.position);
     }
---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> slurbs;
+    public List<GameObject> spawnPoint;
+    private GameObject[] spawn;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+        System.Random randomNumber = new System.Random(GetInstanceID());
+        LoadSpawnPoints();
+        SpawnEnemies();
+    }
+    
+    void LoadSpawnPoints()
+    {
+        spawn = GameObject.FindGameObjectsWithTag("Respawn");
+
+        for (int i = 0; i < spawn.Length; ++i)
+        {
+            spawnPoint.Add(spawn[i]);
+        }
+    }
+
+    void SpawnEnemies()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            int index = Random.Range(0, 16);
+            int enemySpeed = Random.Range(2, 5);
+            Instantiate(prefab, spawnPoint[index].transform.position, Quaternion.identity);
+            print(enemySpeed);
+        }  
+    }
+}
